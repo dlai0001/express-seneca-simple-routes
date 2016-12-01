3644f86b7078392afe5da63ad5ae3d91a5a89867
David Lai <dlai@axs.com>
2016-12-01 14:35:58 -0800

    assert that request and response objects are passed

--- tests/acceptance.js
+++ tests/acceptance.js
@@ -24,20 +24,25 @@ test('it calls next() if route is not handled', function (assert) {
 });
 
 test('it calls seneca action with correct route params if route is matched', function (assert) {
-	assert.plan(1);
-
-	let seneca = require('seneca')();
-
-	seneca.add('role:web,route:userDetail', (msg, reply) => {
-		assert.equal(msg.params.id, '123');
-		return reply(null, {});
-	});
+	assert.plan(4);
 
 	let req = {
 		url: "/user/123"
 	};
 	let next = sinon.spy();
 	let res = sinon.spy();
+
+	let seneca = require('seneca')();
+	seneca.add('role:web,route:userDetail', (msg, reply) => {
+		assert.equal(msg.params.id, '123');
+		assert.equal(msg.reqest, req);
+		assert.equal(msg.response, res);
+		assert.equal(msg.next, next);
+
+		return reply(null, {});
+	});
+
+
 
 	let router = new SimpleRoutes(seneca);
 	router.register({
